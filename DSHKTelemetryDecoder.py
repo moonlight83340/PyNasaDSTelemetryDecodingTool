2805bdf47c9089d48d134c311dcedc88f37b1c64
moonlight83340 <moonlight83340@users.noreply.github.com>
2023-12-19 12:26:24 +0900

    Convert c++ code to python

--- DSHKTelemetryDecoder.py
+++ DSHKTelemetryDecoder.py
@@ -1,33 +1,117 @@
 import struct
-from datetime import datetime, timedelta
+import ctypes
+import datetime
+import Utils
+
+#Maximum length for an absolute path name
+OS_MAX_PATH_LEN = 32
+
+class CCSDS_PrimaryHeader_t(ctypes.Structure):
+    _fields_ = [
+        ('StreamId', ctypes.c_uint8 * 2),  # packet identifier word (stream ID)
+        ('Sequence', ctypes.c_uint8 * 2),  # packet sequence word
+        ('Length', ctypes.c_uint8 * 2)     # packet length word
+    ]
+
+class CFE_MSG_TelemetrySecondaryHeader_t(ctypes.Structure):
+    _fields_ = [
+        ('Time', ctypes.c_uint8 * 6)  # Time, big endian: 4 byte seconds, 2 byte subseconds
+    ]
+    
+class DS_HkTlm_Payload_t(ctypes.Structure):
+    _fields_ = [
+        ('CmdAcceptedCounter', ctypes.c_uint8),
+        ('CmdRejectedCounter', ctypes.c_uint8),
+        ('DestTblLoadCounter', ctypes.c_uint8),
+        ('DestTblErrCounter', ctypes.c_uint8),
+        ('FilterTblLoadCounter', ctypes.c_uint8),
+        ('FilterTblErrCounter', ctypes.c_uint8),
+        ('AppEnableState', ctypes.c_uint8),
+        ('Spare8', ctypes.c_uint8),
+        ('FileWriteCounter', ctypes.c_uint16),
+        ('FileWriteErrCounter', ctypes.c_uint16),
+        ('FileUpdateCounter', ctypes.c_uint16),
+        ('FileUpdateErrCounter', ctypes.c_uint16),
+        ('DisabledPktCounter', ctypes.c_uint32),
+        ('IgnoredPktCounter', ctypes.c_uint32),
+        ('FilteredPktCounter', ctypes.c_uint32),
+        ('PassedPktCounter', ctypes.c_uint32),
+        ('FilterTblFilename', ctypes.c_char * OS_MAX_PATH_LEN)
+    ]
+
+class TelemetrySecondaryHeaderTime:
+    def __init__(self, seconds=0, sub_seconds=0):
+        self.seconds = seconds
+        self.sub_seconds = sub_seconds
+
+def get_telemetry_secondary_header_time(secondary_header):
+    time = TelemetrySecondaryHeaderTime()
+    time.seconds = 0
+    for i in range(4):
+        time.seconds |= secondary_header.Time[i] << ((3 - i) * 8)
+    time.sub_seconds = (secondary_header.Time[4] << 8) | secondary_header.Time[5]
+    return time
+
+def get_packet_datestamp_in_utc_time_from_j2000_time(seconds, sub_seconds):
+    # J2000.0 in UTC
+    j2000_time = datetime.datetime(2000, 1, 1, 11, 58, 55)
+
+    # Add milliseconds to J2000.0 date
+    j2000_time += datetime.timedelta(milliseconds=816)
+
+    # Calculate the date with seconds and subseconds
+    new_time = j2000_time + datetime.timedelta(seconds=seconds, milliseconds=sub_seconds)
+
+    # Display the new date in UTC format
+    utc_time = new_time.strftime("%Y-%m-%d %H:%M:%S")
+    milliseconds = (new_time.microsecond // 1000) % 1000
+    print(f"Date/Time: {utc_time}.{milliseconds} UTC")
+
+    return new_time
 
 class DSHKTelemetryDecoder:
     def __init__(self, file_path):
         self.file_path = file_path
         self.packets = []
 
+    def convert_to_decimal(self, bytes):
+        return (bytes[0] << 8) | bytes[1]
+
     def process_binary_file(self):
         with open(self.file_path, 'rb') as file:
             buffer = file.read()
-
             file_cursor = 0
+
             while file_cursor < len(buffer):
                 current_packet = {}
 
-                primary_header_size = struct.calcsize('!HBBB')
-                primary_header = struct.unpack('!HBBB', buffer[file_cursor:file_cursor + primary_header_size])
+                primary_header_size = ctypes.sizeof(CCSDS_PrimaryHeader_t)
+                primary_header = CCSDS_PrimaryHeader_t.from_buffer_copy(buffer[file_cursor:file_cursor + primary_header_size])
+                current_packet['primary_header'] = primary_header
                 file_cursor += primary_header_size
-                secondary_header_present = primary_header[1] & 0x20
 
+                secondary_header_present = primary_header.StreamId[1] & 0x20
                 if secondary_header_present:
-                    secondary_header_size = struct.calcsize('!4B2H')
-                    secondary_header = struct.unpack('!4B2H', buffer[file_cursor:file_cursor + secondary_header_size])
+                    secondary_header_size = ctypes.sizeof(CFE_MSG_TelemetrySecondaryHeader_t)
+                    secondary_header = CFE_MSG_TelemetrySecondaryHeader_t.from_buffer_copy(buffer[file_cursor:file_cursor + secondary_header_size])
                     file_cursor += secondary_header_size
                     current_packet['secondary_header'] = secondary_header
 
-                payload_size = struct.calcsize('!10I')
-                payload = struct.unpack('!10I', buffer[file_cursor:file_cursor + payload_size])
+                payload_size = ctypes.sizeof(DS_HkTlm_Payload_t)
+                payload = DS_HkTlm_Payload_t.from_buffer_copy(buffer[file_cursor:file_cursor + payload_size])
                 file_cursor += payload_size
+                
+                # Assuming the data is in little-endian, performing conversion to big-endian
+                if Utils.is_little_endian():
+                    payload.FileWriteCounter = Utils.little_to_big_endian_16(payload.FileWriteCounter)
+                    payload.FileWriteErrCounter = Utils.little_to_big_endian_16(payload.FileWriteErrCounter)
+                    payload.FileUpdateCounter = Utils.little_to_big_endian_16(payload.FileUpdateCounter)
+                    payload.FileUpdateErrCounter = Utils.little_to_big_endian_16(payload.FileUpdateErrCounter)
+                    payload.DisabledPktCounter = Utils.little_to_big_endian_32(payload.DisabledPktCounter)
+                    payload.IgnoredPktCounter = Utils.little_to_big_endian_32(payload.IgnoredPktCounter)
+                    payload.FilteredPktCounter = Utils.little_to_big_endian_32(payload.FilteredPktCounter)
+                    payload.PassedPktCounter = Utils.little_to_big_endian_32(payload.PassedPktCounter)
+                
                 current_packet['payload'] = payload
 
                 self.packets.append(current_packet)
@@ -35,19 +119,55 @@ class DSHKTelemetryDecoder:
     def show_packets(self):
         for packet in self.packets:
             print("Packet:")
+            primary_header = packet['primary_header']
+            stream_id = self.convert_to_decimal(primary_header.StreamId)
+            sequence = self.convert_to_decimal(primary_header.Sequence)
+            length = self.convert_to_decimal(primary_header.Length)
+            
+            applicationID = stream_id & 0x07FF
+            secondaryHeaderPresent = (stream_id & 0x0800) >> 11
+            packetType = (stream_id & 0x1000) >> 12
+            ccsdsVersion = (stream_id & 0xE000) >> 13
+
+            sequenceCount = sequence & 0x3FFF
+            segmentationFlags = (sequence & 0xC000) >> 14
+
+            print(f"Application ID: {applicationID}")
+            print(f"Secondary Header: {secondaryHeaderPresent}")
+            print(f"Packet Type: {packetType}")
+            print(f"CCSDS Version: {ccsdsVersion}")
+            print(f"sequence Count: {sequenceCount}")
+            print(f"segmentation Flags: {segmentationFlags}")
+            print(f"length: {length}")
+            
             if 'secondary_header' in packet:
-                print("Secondary Header:", packet['secondary_header'])
-            print("Payload:", packet['payload'])
+                telemetry_time = get_telemetry_secondary_header_time(packet['secondary_header'])
+                print("Seconds:", telemetry_time.seconds)
+                print("Subseconds:", telemetry_time.sub_seconds)
+                utc_date_time = get_packet_datestamp_in_utc_time_from_j2000_time(telemetry_time.seconds, telemetry_time.sub_seconds)
+            payload = packet['payload']
+            print("CmdAcceptedCounter:", payload.CmdAcceptedCounter)
+            print("CmdRejectedCounter:", payload.CmdRejectedCounter)
+            print("DestTblLoadCounter:", payload.DestTblLoadCounter)
+            print("DestTblErrCounter:", payload.DestTblErrCounter)
+            print("FilterTblLoadCounter:", payload.FilterTblLoadCounter)
+            print("FilterTblErrCounter:", payload.FilterTblErrCounter)
+            print("AppEnableState:", payload.AppEnableState)
+            print("FileWriteCounter:", payload.FileWriteCounter)
+            print("FileWriteErrCounter:", payload.FileWriteErrCounter)
+            print("FileUpdateCounter:", payload.FileUpdateCounter)
+            print("FileUpdateErrCounter:", payload.FileUpdateErrCounter)
+            print("DisabledPktCounter:", payload.DisabledPktCounter)
+            print("IgnoredPktCounter:", payload.IgnoredPktCounter)
+            print("FilteredPktCounter:", payload.FilteredPktCounter)
+            print("PassedPktCounter:", payload.PassedPktCounter)
+            print("FilterTblFilename:", payload.FilterTblFilename)
 
-    @staticmethod
-    def get_packet_datestamp_in_utc_time_from_j2000_time(seconds, sub_seconds):
-        j2000_epoch = datetime(year=2000, month=1, day=1, hour=11, minute=58, second=55)
-        j2000_time = j2000_epoch + timedelta(seconds=seconds, milliseconds=sub_seconds)
-        utc_time = j2000_time.strftime('%Y-%m-%d %H:%M:%S.%f') + " UTC"
-        return utc_time
-
-# Example usage
-file_path = "C:/Users/perrotg/Documents/GitHub/NasaDSTelemetryDecodingTool/TestFile/ds_tlm.bin"
+if Utils.is_little_endian():
+    print("The machine is little endian")
+else:
+    print("The machine is big endian")
+file_path = "C:/Users/perrotg/fork/PyNasaDSTelemetryDecodingTool/TestFile/ds_tlm.bin"
 decoder = DSHKTelemetryDecoder(file_path)
 decoder.process_binary_file()
 decoder.show_packets()
--- Utils.py
+++ Utils.py
@@ -9,13 +9,3 @@ def little_to_big_endian_32(value):
 def is_little_endian():
     test_value = 0x12345678
     return sys.byteorder == 'little'
-
-# Exemple d'utilisation
-test_number = 0x1234
-converted_number = little_to_big_endian_16(test_number)
-print(f"Original: {hex(test_number)}, Converted: {hex(converted_number)}")
-
-if is_little_endian():
-    print("The machine is little endian")
-else:
-    print("The machine is big endian")
